68ad8d9fd51e069b7feb71619d2450fd410d7d08
Dan Woodson <woodson.dan@gmail.com>
2019-02-02 16:38:55 -0700

    PouchDB setup

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,12 +1,19 @@
 // @flow
 
 import * as React from 'react'
+import PouchDB from 'pouchdb'
 
 /**
  * [COMP DESCRIPTION]
  * @param {object} props - Component properties
  * @return {JSX} - Component
  */
+
+const db = new PouchDB(process.env.DB_URL, {
+  auth: { username: process.env.DB_USER, password: process.env.DB_PASS }
+})
+
+db.info().then(console.log)
 
 type Props = {||}
 
