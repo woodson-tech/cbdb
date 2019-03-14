77c4db92f90dfa6bb3afb8d4dc8de935cfef4461
Jennifer Woodson <jenniferpwoodson@gmail.com>
2019-03-13 20:27:41 -0600

    Try to connect to db

--- src/containers/index.js
+++ src/containers/index.js
@@ -11,11 +11,29 @@ import sampleIssue from './../dummydata.json'
  * @return {JSX} - Component
  */
 
+ console.log('url: ', process.env.DB_URL)
 const db = new PouchDB(process.env.DB_URL, {
   auth: { username: process.env.DB_USER, password: process.env.DB_PASS }
 })
+console.log('db')
+console.log(db)
 
-db.info().then(console.log)
+db.info().then(result => {
+  console.log('db info')
+  console.log(result)
+})
+
+db.allDocs().then(result => {
+  console.log('all docs')
+  console.log(result)
+})
+
+db.get('ac78f6f1ed5732748818609cc60005b7')
+  .then(result => {
+  console.log('doc')
+  console.log(result)
+})
+.catch(error => console.log('error: ', error))
 
 console.log('in containers/index.js', sampleIssue)
 
